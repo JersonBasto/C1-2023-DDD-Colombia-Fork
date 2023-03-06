88062d37d1b0a79ca2a0bc88ac44cc7dade14c28
JersonBasto <90776343+JersonBasto@users.noreply.github.com>
2023-03-05 23:33:33 -0500

    Se arregla el retorno
    Se arregla el retorno en la entidad de servicios del gate.

--- src/subdomains/house-machines/contexts/emergencies/domain/services/gate.domain-service.ts
+++ src/subdomains/house-machines/contexts/emergencies/domain/services/gate.domain-service.ts
@@ -3,6 +3,6 @@ import { GateDomainEntity } from '../entities/gate.domain-entity';
 export interface IGateDomainService<Entity extends GateDomainEntity> {
   openGates(): void;
   closeGates(): void;
-  changeStateGate(value: boolean): GateDomainEntity;
-  changeStateEmergency(value: boolean): GateDomainEntity;
+  changeStateGate(value: boolean): Promise<Entity>;
+  changeStateEmergency(value: boolean): Promise<Entity>;
 }
