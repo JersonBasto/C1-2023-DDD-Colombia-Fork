993a8cd144aba7d6988f1db4a4d2a9933fd0af38
JersonBasto <90776343+JersonBasto@users.noreply.github.com>
2023-03-05 23:32:27 -0500

    Se agregan dos metodos nuevos
    Se agregan dos metodos nuevos en el servicio de Gate

--- src/subdomains/house-machines/contexts/emergencies/domain/services/gate.domain-service.ts
+++ src/subdomains/house-machines/contexts/emergencies/domain/services/gate.domain-service.ts
@@ -3,4 +3,6 @@ import { GateDomainEntity } from '../entities/gate.domain-entity';
 export interface IGateDomainService<Entity extends GateDomainEntity> {
   openGates(): void;
   closeGates(): void;
+  changeStateGate(value: boolean): GateDomainEntity;
+  changeStateEmergency(value: boolean): GateDomainEntity;
 }
