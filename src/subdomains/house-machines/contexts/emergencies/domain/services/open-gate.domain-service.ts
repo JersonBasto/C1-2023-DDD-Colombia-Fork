a187c088583953cebd76c5ef7d0d359048158306
JersonBasto <90776343+JersonBasto@users.noreply.github.com>
2023-03-05 23:34:48 -0500

    Se arregla el retorno
    Se arregla el retorno de la entidad OpenGate Service.

--- src/subdomains/house-machines/contexts/emergencies/domain/services/open-gate.domain-service.ts
+++ src/subdomains/house-machines/contexts/emergencies/domain/services/open-gate.domain-service.ts
@@ -1,6 +1,6 @@
 import { IOpenGateDomainEntity } from '../entities/interfaces/open-gate.domain-entity.interface';
 
 export interface IOpenGateDomainService<Entity extends IOpenGateDomainEntity> {
-  registerOpenAction(data: IOpenGateDomainEntity): IOpenGateDomainEntity;
-  getHistoryOpenAction(): IOpenGateDomainEntity[];
+  registerOpenAction(data: IOpenGateDomainEntity): Promise<Entity>;
+  getHistoryOpenAction(): Promise<Entity[]>;
 }
